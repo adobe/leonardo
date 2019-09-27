7478358d8f7a513ac077eb0700b94a8c61fd10f2
Nate Baldwin <nbaldwin@adobe.com>
2019-09-27 15:55:49 -0600

    added gulp and sass for importing spectrum-css partials

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
