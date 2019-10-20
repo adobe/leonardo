df3807360a04550df06c49f83d1e411cd6488594
Nate Baldwin <nbaldwin@adobe.com>
2019-10-20 02:31:40 -0600

    generate demo blocks, output contrast ratio in swatches

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ var demoButtonInverted = document.getElementById('demoButtonInverted');
 var userColorBlock = document.getElementById('userColor');
 var userBgBlock = document.getElementById('userBg');
 var ratioInput = document.getElementById('ratio');
-var targetRatio = ratioInput.value;
+// var targetRatio = ratioInput.value;
 // var scaleNumbers = document.querySelector('input[name="scaleNumbers"]:checked').value;
 var colorOutputField = document.getElementById('colorOutput');
-var fieldColorOutput = document.getElementById('spectrum-Textfield-swatch');
+// var fieldColorOutput = document.getElementById('spectrum-Textfield-swatch');
 var swatches = 500; // in order to make a gradient, this count needs to be massive
 
 function paramSetup() {
@@ -48,26 +48,36 @@ function paramSetup() {
   if(params.has('shade')) {
     document.getElementById('colorField3').value = "#" + params.get('shade');
   }
+  if(params.has('ratios') !== true) {
+    addRatio(3);
+    addRatio(4.5);
+  }
+  // if(params.has('ratios')) {
+  //   rat = params.get('ratios');
+  //   ratios = [];
+  //   ratios.push(rat);
+  //
+  //   for(i=0; i>ratios.length; i++) {
+  //     addRatio(ratios[i]);
+  //     // console.log(ratios[i]);
+  //   }
+  // }
   if(params.has('mode')) {
     document.querySelector('select[name="mode"]').value = params.get('mode');
   }
-  // // TODO: Won't work until I have ratioUpdate() function working
-  if(params.has('ratio')) {
-    var contrastRatio2 = params.get('ratio');
-
-    console.log(constrastRatio2);
-  } else { }
+  else { }
 }
 paramSetup();
 
 function colorblock(c){
   colorBlock.style.backgroundColor = c;
-  demoBackgroundBlock.style.backgroundColor = c;
-  demoText.style.color = c;
-  demoHeading.style.color = c;
-  demoButton.style.color = c;
-  demoButton.style.borderColor = c;
-  fieldColorOutput.style.backgroundColor = c;
+  // demoBackgroundBlock.style.backgroundColor = c;
+  // demoText.style.color = c;
+  // demoHeading.style.color = c;
+  // demoButton.style.color = c;
+  // demoButton.style.borderColor = c;
+  // document.getElementById(inputId + '-sw');
+  // fieldColorOutput.style.backgroundColor = c;
 }
 colorblock(color1);
 
@@ -82,23 +92,25 @@ function backgroundblock(b){
 backgroundblock(background);
 
 // Add ratio inputs
-function addRatio(v) {
+function addRatio(v = 1) {
   var ratios = document.getElementById('ratios');
   var div = document.createElement('div');
+  var randId = randomId();
   div.className = 'ratio-Item';
   div.id = randomId();
   var sw = document.createElement('span');
   sw.className = 'spectrum-Textfield-swatch';
+  sw.id = randId + '-sw';
   var input = document.createElement('input');
   input.className = 'spectrum-Textfield ratioField';
   input.type = "number";
   input.min = '1';
   input.max = '21';
-  input.step = '.01'
-  var randId = randomId();
+  input.step = '.01';
+  input.placeholder = 4.5;
   input.id = randId;
   input.value = v;
-  input.onchange = colorInput;
+  input.oninput = colorInput;
   input.onfocus = showSlider;
   input.onblur = hideSlider;
   var button = document.createElement('button');
@@ -111,25 +123,26 @@ function addRatio(v) {
   var xlink = document.createElementNS('http://www.w3.org/2000/svg', 'use');
   xlink.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#spectrum-icon-18-Delete'); // this doesn't work
 
-  // TODO: create new slider
-  createSlider(randId);
+  createSlider(randId, v);
 
   icon.appendChild(xlink);
   button.appendChild(icon);
-  // button.addEventListener('click', deleteRatio('test_01'));
   button.onclick = deleteRatio;
   div.appendChild(sw);
   div.appendChild(input);
   div.appendChild(button);
   ratios.appendChild(div);
 }
-addRatio(3); // For testing
-addRatio(4.5); // For testing
+// addRatio(3); // For testing
+// addRatio(4.5); // For testing
 
 // Delete ratio input
 function deleteRatio(e) {
   var id = e.target.parentNode.id;
   var self = document.getElementById(id);
+  var sliderid = id + '-slider';
+  var slider = document.getElementById(sliderid);
+
   self.remove();
 }
 // random id's
@@ -137,15 +150,17 @@ function randomId() {
    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
 }
 
-function createSlider(x) {
+function createSlider(x, v) {
   var sliderWrapper = document.getElementById('sliderWrapper');
   var slider = document.createElement('input');
   slider.type = 'range';
-  slider.min = '0';
-  slider.max = '500';
-  slider.oninput = colorInput;
+  slider.min = '1';
+  slider.max = '20';
+  slider.value = v;
+  slider.step = '.01';
+  slider.oninput = syncInputVal;
   slider.className = 'slider'
-  slider.id = x;
+  slider.id = x + "-sl";
   slider.style.display = 'none';
 
   sliderWrapper.appendChild(slider);
@@ -153,8 +168,8 @@ function createSlider(x) {
 
 function showSlider() {
   var id = this.id;
-  var slider = document.getElementById(id);
-  if (this.focus) {
+  var slider = document.getElementById(id + '-sl');
+  if (this.focus || slider.focus) {
     slider.style.display = 'block';
   } else {
     slider.style.display = 'none';
@@ -162,18 +177,95 @@ function showSlider() {
 }
 function hideSlider() {
   var id = this.id;
-  var slider = document.getElementById(id);
-  if (this.focus !== true) {
+  var slider = document.getElementById(id + '-sl');
+  if (this.focus !== true && slider.focus !== true) {
     slider.style.display = 'none';
+  } else {
+    slider.style.display = 'block';
   }
+}
+function syncVal() {
+  id = this.id;
+  slider = document.getElementById(id + '-sl');
+  c = this.value;
+  slider.value = c;
+}
+function syncInputVal() {
+  id = this.id;
+  inputId = id.substring(0, id.length - 3);
+  input = document.getElementById(inputId);
+  // TODO: this should not be done this way.
+  // Slider should use previous method of deterimining
+  // position relative to color domain of swatches.
+  // In this function, then calculate ratio & return ratio as value input?
+  input.value = this.value;
+}
+
+function createDemo(c, z) {
+  wrap = document.getElementById('demoWrapper');
+  item = document.createElement('div');
+  item.className = 'demoItem';
+  demo = document.createElement('div');
+  demo.className = 'spectrum-Typography demo';
+  h = document.createElement('h4');
+  h.className = 'spectrum-Heading2 demoHeading';
+  title = document.createTextNode('Large text');
+  p = document.createElement('p');
+  p.className = 'spectrum-Body3 demoText';
+  text = document.createTextNode('Small text demonstration');
+  b = document.createElement('button');
+  b.className = 'spectrum-Button demoButton';
+  label = document.createTextNode('Button');
+
+  h.appendChild(title);
+  p.appendChild(text);
+  b.appendChild(label);
+  demo.appendChild(h);
+  demo.appendChild(p);
+  demo.appendChild(b);
+
+  demoIn = document.createElement('div');
+  demoIn.className = 'spectrum-Typography demoInverted';
+  hIn = document.createElement('h4');
+  hIn.className = 'spectrum-Heading2 demoHeading';
+  pIn = document.createElement('p');
+  pIn.className = 'spectrum-Body3 demoText';
+  bIn = document.createElement('button');
+  bIn.className = 'spectrum-Button demoButton';
+  titleIn = document.createTextNode('Large text');
+  textIn = document.createTextNode('Small text demonstration');
+  labelIn = document.createTextNode('Button');
+
+  hIn.appendChild(titleIn);
+  pIn.appendChild(textIn);
+  bIn.appendChild(labelIn);
+  demoIn.appendChild(hIn);
+  demoIn.appendChild(pIn);
+  demoIn.appendChild(bIn);
+
+  item.appendChild(demo);
+  item.appendChild(demoIn);
+  wrap.appendChild(item);
+
+  demoIn.style.backgroundColor = c;
+  demoIn.style.color = z;
+  demo.style.color = c;
+  p.style.color = c;
+  h.style.color = c;
+  b.style.color = c;
+  b.style.borderColor = c;
+  pIn.style.color = z;
+  hIn.style.color = z;
+  bIn.style.color = z;
+  bIn.style.borderColor = z;
 }
 
 // Calculate Color and generate Scales
 function colorInput() {
   document.getElementById('colors').innerHTML = '';
 
-  var slider = document.getElementById('Slider');
-  var background = document.getElementById('bgField').value;
+  // var slider = document.getElementById('Slider');
+  background = document.getElementById('bgField').value;
   var customTintShade = document.getElementById('tintShades');
   var color1 = colorField1.value;
   var colorTint = colorField2.value;
@@ -187,9 +279,9 @@ function colorInput() {
   for(i=0; i < ratioFields.length; i++) {
     ratioInputs.push(ratioFields[i].value);
   }
+  console.log(ratioInputs);
 
   adaptcolor({color: color1, base: background, ratios: ratioInputs, tint: colorTint, shade: colorShade, colorspace: mode});
-  console.log(newColors);
 
   for(i=0; i<newColors.length; i++) {
     colorblock(newColors[i])
@@ -212,45 +304,46 @@ function colorInput() {
   backgroundblock(background);
 
   // Slider updates
-  var sliderPos = document.getElementById('Slider').value;
-  var colorDomainUpdate =  swatches - (swatches * sliderPos /500);
-  var newRgb = colors[colorDomainUpdate];
-  var contrastRatio2 = contrast([backgroundR, backgroundG, backgroundB], [d3.rgb(newRgb).r, d3.rgb(newRgb).g, d3.rgb(newRgb).b]).toFixed(2);
+  // var sliderPos = document.getElementById('Slider').value;
+  // var colorDomainUpdate =  swatches - (swatches * sliderPos /500);
+  // var newRgb = colors[colorDomainUpdate];
+  // var contrastRatio2 = contrast([backgroundR, backgroundG, backgroundB], [d3.rgb(newRgb).r, d3.rgb(newRgb).g, d3.rgb(newRgb).b]).toFixed(2);
 
-  newHex = d3.rgb(newRgb).formatHex();
+  // newHex = d3.rgb(newRgb).formatHex();
   // colorOutputField.value = newRgb + '\n' + newHex + '\n' + contrastRatio2 + ":1";
   var colorOutputWrapper = document.getElementById('colorOutputs');
-  var colorOutput = document.createElement('div');
-  var colorOutputVal = newHex;
-  var colorOutputText = document.createTextNode(colorOutputVal);
   colorOutputWrapper.innerHTML = '';
-  colorOutputWrapper.appendChild(colorOutput);
-  colorOutput.className = 'colorOutputBlock';
-  colorOutput.style.backgroundColor = color1;
-  colorOutput.style.backgroundColor = newRgb;
-  colorOutput.appendChild(colorOutputText);
+  wrap = document.getElementById('demoWrapper');
+  wrap.innerHTML = '';
 
-  colorblock(newRgb);
-  colorBlock.style.bottom = sliderPos / 5 + "%";
-  slider.value = sliderPos;
-  ratioInput.value = contrastRatio2;
-  var colorR = d3.rgb(newRgb).r;
-  var colorG = d3.rgb(newRgb).g;
-  var colorB = d3.rgb(newRgb).b;
-  if (luminance(colorR, colorG, colorB) < 0.275) {
-    colorBlock.style.color = "#ffffff";
-    colorOutput.style.color = "#ffffff";
-  } else {
-    colorBlock.style.color = '#000000';
-    colorOutput.style.color = '#000000';
+  for (i = 0; i < newColors.length; i++) {
+    var colorOutput = document.createElement('div');
+    var colorOutputVal = newColors[i];
+    var colorOutputText = document.createTextNode(d3.rgb(colorOutputVal).hex());
+    var bg = d3.color(background).rgb();
+    var outputRatio = contrast([d3.rgb(newColors[i]).r, d3.rgb(newColors[i]).g, d3.rgb(newColors[i]).b], [bg.r, bg.g, bg.b]);
+    var ratioText = document.createTextNode(outputRatio.toFixed(2));
+    var s1 = document.createElement('span');
+    var s2 = document.createElement('span');
+
+    colorOutputWrapper.appendChild(colorOutput);
+    colorOutput.className = 'colorOutputBlock';
+    colorOutput.style.backgroundColor = colorOutputVal;
+    s1.appendChild(colorOutputText);
+    s2.appendChild(ratioText);
+    colorOutput.appendChild(s1);
+    colorOutput.appendChild(s2);
+
+    if (luminance(d3.rgb(newColors[i]).r, d3.rgb(newColors[i]).g, d3.rgb(newColors[i]).b) < 0.275) {
+      colorOutput.style.color = "#ffffff";
+    } else {
+      colorOutput.style.color = '#000000';
+    }
+    createDemo(newColors[i], background);
   }
-  var textUpdate = document.createTextNode(contrastRatio2);
-
-  colorBlock.innerHTML = '';
-  colorBlock.appendChild(textUpdate);
 
   // update URL parameters
-  // updateParams(color1.substr(1), background.substr(1), colorTint.substr(1), colorShade.substr(1), contrastRatio2, mode, 'd3');
+  updateParams(color1.substr(1), background.substr(1), colorTint.substr(1), colorShade.substr(1), ratioInputs, mode, 'd3');
 }
 colorInput(color1);
 
@@ -260,11 +353,11 @@ function updateParams(c, b, t, s, r, m, l) {
   let url = new URL(window.location);
   let params = new URLSearchParams(url.search.slice(1));
 
-  params.set('base', b);
   params.set('color', c);
-  params.set('ratio', r);
+  params.set('base', b);
   params.set('tint', t);
   params.set('shade', s);
+  params.set('ratios', r);
   params.set('mode', m);
   // TODO: uncomment when integrated with library options
   // params.set('lib', l);
@@ -273,7 +366,28 @@ function updateParams(c, b, t, s, r, m, l) {
 
   var p = document.getElementById('params');
   p.innerHTML = " ";
-  var z = 'adaptcolor({color: "#' + c + '", base: "#'+ b + '", ratios: [' + r + '], ' + 'tint: "#' + t + '", shade: "#' + s + '", ' + ' colorspace: "' + m + '", lib: "' + l + '"});';
-  text = document.createTextNode(z);
-  p.appendChild(text);
+  var call = 'adaptcolor({';
+  var pcol = 'color: "#' + c + '",';
+  var pbas = 'base: "#'+ b + '",';
+  var prat = 'ratios: [' + r + '],';
+  var ptin = 'tint: "#' + t + '",';
+  var psha = 'shade: "#' + s + '",';
+  var pmod = ' colorspace: "' + m + '",';
+  var plib = 'lib: "' + l + '"});';
+  text1 = document.createTextNode(call);
+  text2 = document.createTextNode(pcol);
+  text3 = document.createTextNode(pbas);
+  text4 = document.createTextNode(prat);
+  text5 = document.createTextNode(ptin);
+  text6 = document.createTextNode(psha);
+  text7 = document.createTextNode(pmod);
+  text8 = document.createTextNode(plib);
+  p.appendChild(text1);
+  p.appendChild(text2);
+  p.appendChild(text3);
+  p.appendChild(text4);
+  p.appendChild(text5);
+  p.appendChild(text6);
+  p.appendChild(text7);
+  p.appendChild(text8);
 }
