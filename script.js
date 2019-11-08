eba38a0c81e3d8151d5263f4e21646cea7ff27e5
Nate Baldwin <nbaldwin@adobe.com>
2019-11-08 09:36:09 -0700

    implemented tab interaction for panel

--- script.js
+++ script.js
@@ -193,6 +193,31 @@ function deleteColor(e) {
   colorInput();
 }
 
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="spectrum-Tabs-item" and remove the class "active"
+  tablinks = document.getElementsByClassName("spectrum-Tabs-item");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" is-selected", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "flex";
+  evt.currentTarget.className += " is-selected";
+}
+
+// Open default tabs
+document.getElementById("tabColorScale").click();
+
+
 function randomId() {
    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
 }
