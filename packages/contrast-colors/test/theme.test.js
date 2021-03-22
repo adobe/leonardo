4180869c7e73c7b26d5b6ae6afcff6ef44f7301b
Nate Baldwin <nbaldwin@adobe.com>
2021-03-22 11:52:35 -0600

    adding tests for single color

--- packages/contrast-colors/test/theme.test.js
+++ packages/contrast-colors/test/theme.test.js
@@ -495,4 +495,374 @@ test('should generate colors with user-defined names and increased contrast', fu
   ]);
 });
 
+/** 
+ * Single-color theme tests with fixed backround color.
+ * These tests are for the new method of generating contrast
+ * colors for a single color scale with a non-scale-based 
+ * background. Replaces `new Color` function
+ */
 
+// Test simple generation in all color spaces
+test('should generate 2 colors (CAM02 interpolation)', function() {
+  let gray = new Color({
+    name: 'gray',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'],
+    colorspace: 'CAM02',
+    ratios: [3, 4.5]
+  });
+  let theme = new Theme({colors: [gray], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#5490e0', '#2c66f1' ]);
+
+});
+
+test('should generate 2 named colors (CAM02 interpolation)', function() {
+  let cerulean = new Color({
+    name: 'Cerulean', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'CAM02'
+  });
+  let theme = new Theme({colors: [cerulean], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#5490e0', '#2c66f1' ]);
+
+});
+
+test('should generate 2 colors (LAB interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'LAB'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#7383ff', '#435eff' ]);
+});
+
+test('should generate 2 colors (LCH interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'LCH'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#008fff', '#0065ff' ]);
+});
+
+test('should generate 2 colors (HSL interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'HSL'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#478cfe', '#2d62ff' ]);
+});
+
+test('should generate 2 colors (HSLuv interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'HSLuv'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#1896dc', '#066aea' ]);
+});
+
+test('should generate 2 colors (HSV interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'HSV'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#478cff', '#2d62ff' ]);
+});
+
+test('should generate 2 colors (RGB interpolation)', function() {
+  let indigo = new Color({
+    name: 'Indigo',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: 'RGB'
+  });
+  let theme = new Theme({colors: [indigo], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#5988ff', '#3360ff' ]);
+});
+
+test('should generate 2 colors on dark background', function() {
+  let color = new Color({
+    name: 'Color',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [3, 4.5], 
+    colorspace: "LCH"
+  }); 
+  let theme = new Theme({colors: [color], backgroundColor: '#323232'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#0074ff', '#009fff' ]);
+});
+
+// Check bidirectionality of contrast ratios (positive vs negative)
+test('should generate 2 colors with bidirectional contrast (light background)', function() {
+  let color = new Color({
+    name: 'Color',
+    colorKeys: ["#012676"], 
+    ratios: [-1.25,4.5], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#D8D8D8'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#efeff6', '#56599a' ]);
+});
+
+test('should generate 2 colors with bidirectional contrast (dark background)', function() {
+  let color = new Color({
+    name: 'Color',
+    colorKeys: ["#012676"], 
+    base: "#323232",
+    ratios: [-1.25,4.5], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#323232'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#101c51', '#9695c0' ]);
+});
+
+// Contrast gamuts
+test('should generate black when ratio darker than available colors', function() {
+  let color = new Color({
+    name: 'Color',
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    base: "#d8d8d8",
+    ratios: [21], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#d8d8d8'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#000000' ]);
+});
+
+test('should generate white when ratio lighter than available colors', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [21], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#323232'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#ffffff' ]);
+});
+
+test('should generate white when negative ratio lighter than available colors', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [-21], 
+    colorspace: "LCH"
+  }); 
+  let theme = new Theme({colors: [color], backgroundColor: '#f5f5f5'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#ffffff' ]);
+});
+test('should generate white when negative ratio lighter than available colors', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [-21], 
+    colorspace: "LCH"
+  }); 
+  let theme = new Theme({colors: [color], backgroundColor: '#ffffff'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#ffffff' ]);
+});
+
+test('should generate black when negative ratio lighter than available colors', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [-21], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#323232'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#000000' ]);
+});
+
+test('should generate black when negative ratio lighter than available colors', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#2451FF', '#C9FEFE', '#012676'], 
+    ratios: [-21], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#000000'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#000000' ]);
+});
+
+// Mid-Tone Backgrounds
+test('should generate slightly lighter & darker grays on a darker midtone gray background', function() {
+  let color = new Color({
+    name: 'Color',
+    colorKeys: ['#000000'], 
+    ratios: [1.2, -1.2], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#737373'}); 
+  let themeColors = theme.contrastColorValues;
+
+  expect(themeColors).toEqual([ '#808080', '#666666' ]);
+});
+test('should generate slightly lighter & darker grays on a lighter midtone gray background', function() {
+  let color = new Color({
+    name: 'Color', 
+    colorKeys: ['#000000'], 
+    ratios: [1.2, -1.2], 
+    colorspace: "LCH"
+  }); // positive & negative ratios
+  let theme = new Theme({colors: [color], backgroundColor: '#787878'}); 
+  let themeColors = theme.contrastColors;
+
+  // expect(themeColors).toEqual([ '#6b6b6b', '#858585' ]);
+  expect(themeColors).toEqual([
+    { "background": "#787878" },
+    {
+      "name": 'Color',
+      "values": [
+        {"name": "Color50", "contrast": -1.2, "value": "#6b6b6b"},
+        {"name": "Color100", "contrast": 1.2, "value": "#858585"},
+      ]
+    }
+  ]);
+});
+// test('should generate slightly lighter & darker oranges on a darker midtone slate background', function() {
+//   let color = new Color({
+//     name: 'Color', 
+//     colorKeys: ["#ff8602","#ab3c00","#ffd88b"], 
+//     ratios: [1.2, -1.2], 
+//     colorspace: "LCH"
+//   }); // positive & negative ratios
+//   let theme = new Theme({colors: [color], backgroundColor: '#537a9c'}); 
+//   let themeColors = theme.contrastColorValues;
+
+//   expect(themeColors).toEqual([ '#d66102', '#b64601' ]);
+// });
+// test('should generate slightly lighter & darker oranges on a lighter midtone slate background', function() {
+//   let color = new Color({
+//     name: 'Color', 
+//     colorKeys: ["#ff8602","#ab3c00","#ffd88b"], 
+//     ratios: [1.2, -1.2], 
+//     colorspace: "LCH"
+//   }); // positive & negative ratios
+//   let theme = new Theme({colors: [color], backgroundColor: '#537a9c'}); 
+//   let themeColors = theme.contrastColorValues;
+
+//   expect(themeColors).toEqual([ '#b84601', '#d76202' ]);
+// });
+
+
+
+
+
+// // Output formats 
+// test('should generate 2 colors in HEX format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02'});;
+
+//   expect(colors).toEqual([ '#5490e0', '#2c66f1' ]);
+// });
+// test('should generate 2 colors in RGB format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02', output: 'RGB'});;
+
+//   expect(colors).toEqual([ 'rgb(84, 144, 224)', 'rgb(44, 102, 241)' ]);
+// });
+// test('should generate 2 colors in HSL format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02', output: 'HSL'});;
+
+//   expect(colors).toEqual([ 'hsl(214deg, 69%, 60%)', 'hsl(222deg, 88%, 56%)' ]);
+// });
+// test('should generate 2 colors in HSV format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02', output: 'HSV'});;
+
+//   expect(colors).toEqual([ 'hsv(214deg, 63%, 88%)', 'hsv(222deg, 82%, 95%)' ]);
+// });
+// test('should generate 2 colors in LAB format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02', output: 'LAB'});;
+
+//   expect(colors).toEqual([ 'lab(58%, -1, -47)', 'lab(46%, 22, -77)' ]);
+// });
+// test('should generate 2 colors in LCH format', function() {
+//   let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5', ratios: [3, 4.5], colorspace: 'CAM02', output: 'LCH'});;
+
+//   expect(colors).toEqual([ 'lch(58%, 47, 269deg)', 'lch(46%, 80, 286deg)' ]);
+// });
+
+// // Expected errors
+// test('should generate no colors, missing colorKeys', function() {
+//   expect(
+//     () => {
+//       let color = new Color({base: '#f5f5f5', ratios: [3, 4.5]}) // no key colors
+//     }
+//   ).toThrow();
+// });
+
+// test('should generate no colors, missing ratios', function() {
+
+//   expect(
+//     () => {
+//       let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], base: '#f5f5f5'}) // no ratios
+//     }
+//   ).toThrow();
+// });
+
+// test('should generate no colors, missing base', function() {
+//   expect(
+//     () => {
+//       let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEFE', '#012676'], ratios: [3, 4.5]}) // no base
+//     }
+//   ).toThrow();
+// });
+
+// test('should throw error, missing hash on hex value', function() {
+//   expect(
+//     () => {
+//       let color = new Color({name: 'Color', colorKeys: ['#2451FF', 'C9FEFE', '#012676'], ratios: [3, 4.5]}) // third color missing hash #
+//     }
+//   ).toThrow();
+// });
+
+// test('should throw error, incomplete hex value', function() {
+//   expect(
+//     () => {
+//       let color = new Color({name: 'Color', colorKeys: ['#2451FF', '#C9FEF', '#012676'], ratios: [3, 4.5]}) // third color missing final hex code
+//     }
+//   ).toThrow();
+// });
