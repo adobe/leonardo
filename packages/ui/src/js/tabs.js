b02ba2dc79a01d13dd8f65006f136c8b66ad8c5c
Nate Baldwin <nbaldwin@adobe.com>
2022-01-19 09:35:39 -0700

    temporarily removed sidenav anchor links from API docs page

--- packages/ui/src/js/tabs.js
+++ packages/ui/src/js/tabs.js
@@ -9,6 +9,8 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+const { stackOrderNone } = require("d3");
+
 function openPanelTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
