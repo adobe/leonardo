57ad463a94bb7af728802bd5dd9d2cf1a4b2fc4b
rsek <5354757+rsek@users.noreply.github.com>
2023-03-01 13:30:34 -0800

    improve inline documentation

--- packages/contrast-colors/index.d.ts
+++ packages/contrast-colors/index.d.ts
@@ -12,19 +12,69 @@ governing permissions and limitations under the License.
 
 import type ChromaJs from 'chroma-js'
 
-type Colorspace = 'CAM02' | 'CAM02p' | 'HEX' | 'HSL' | 'HSLuv' | 'HSV' | 'LAB' | 'LCH' | 'RGB' | 'OKLAB' | 'OKLCH'
+/**
+ * A valid CSS hexadecimal color.
+ * @remarks This could theoretically be specified with a more precise string template, but in practice, TS attempts to interpolate every possible combination of digits.
+ */
+type HexColor = `#${string}`
+
+/**
+ * Supported colorspaces from the {@link https://www.w3.org/TR/css-color-4/ W3C CSS Color Module Level 4} spec.
+ * @example '#RRGGBB' // HEX
+ * @example 'rgb(255, 255, 255)' // RGB
+ * @example 'hsl(360deg, 0 %, 100 %)' // HSL
+ * @example 'hsv(360deg, 0 %, 100 %)' // HSV
+ * @example 'hsluv(360, 0, 100)' // HSLuv
+ * @example 'lab(100 %, 0, 0)' // LAB
+ * @example 'lch(100 %, 0, 360deg)' // LCH
+ * @example 'jab(100 %, 0, 0)' // CAM02
+ * @example 'jch(100 %, 0, 360deg)' // CAM02p
+ */
+type Colorspace =
+  | 'HEX'
+  | 'RGB'
+  | 'HSL'
+  | 'HSV'
+  | 'HSLuv'
+  | 'LAB'
+  | 'LCH'
+  | 'CAM02'
+  | 'CAM02p'
+
+/**
+ * Supported interpolation colorspaces from the {@link https://www.w3.org/TR/css-color-4/ W3C CSS Color Module Level 4} spec.
+ */
+type InterpolationColorspace = Exclude<Colorspace, 'HEX'>
 
 type RGBArray = [number, number, number]
 
 interface ColorBase {
+  /**
+   * User-defined name for a color, (eg "Blue"). Used to name output color values.
+   */
   name: string
   /**
-   * Colors to be validated by {@link ChromaJs.valid}
+   * List of specific colors to interpolate between in order to generate a full lightness scale of the color.
+   *
+   * @remarks Strings are passed to {@link ChromaJs.valid}
    */
-  colorKeys: any[] // FIXME
-  colorspace?: Colorspace
-  ratios: Ratios
+  colorKeys: HexColor[]
+  /**
+   * The colorspace in which the key colors will be interpolated.
+   */
+  colorspace?: InterpolationColorspace
+  /**
+   * List of target contrast ratios, or object with named keys for each value.
+   * @see {@link RatiosArray}, {@link RatiosObject}
+   */
+  ratios: RatiosArray | RatiosObject
+  /**
+   * Applies bezier smoothing to interpolation.
+   */
   smooth?: boolean
+  /**
+   * Desired color output format.
+   */
   output?: Colorspace
   saturation?: number
 }
@@ -32,8 +82,8 @@ interface ColorBase {
 export class Color implements Required<ColorBase> {
   constructor({ name, colorKeys, colorspace, ratios, smooth, output, saturation }: ColorBase)
   name: string
-  colorKeys: any[]
-  colorspace: Colorspace
+  colorKeys: HexColor[]
+  colorspace: InterpolationColorspace
   ratios: Ratios
   smooth: boolean
   output: Colorspace
@@ -44,24 +94,28 @@ export class Color implements Required<ColorBase> {
 export class BackgroundColor extends Color {
   readonly backgroundColorScale: ChromaJs.Scale
 }
-type ColorLike = string | number | ChromaJs.Color
+/**
+ * @see {@link https://www.w3.org/TR/WCAG22/#contrast-minimum}
+ * @see {@link https://www.w3.org/TR/wcag-3.0/#visual-contrast-of-text}
+ */
 type ContrastFormula = 'wcag2' | 'wcag3'
 type LightnessDistribution = 'linear' | 'polynomial'
 
 /**
  * Helper function for rounding color values to whole numbers.
  */
-export function convertColorValue(color: ColorLike, format: Colorspace,
+export function convertColorValue(color: string, format: Colorspace,
   /** @default false */
   object?: boolean): number
 
 export function createScale({ swatches, colorKeys, colorspace, shift, fullScale, smooth, distributeLightness, sortColor, asFun, }?: {
-  /** The number of swatches in the scale. */
+  /** The number of swatches/steps in the scale. */
   swatches: number
-  /** Colors passed to {@link chroma} */
-  colorKeys: string[]
-  /** @default 'LAB' */
-  colorspace?: Colorspace
+  colorKeys: HexColor[]
+  /**
+   * The colorspace used to interpolate the color scale.
+   * @default 'LAB' */
+  colorspace?: InterpolationColorspace
   /** @default 1 */
   shift?: number
   /** @default true */
@@ -83,10 +137,6 @@ export function contrast(color: RGBArray, base: RGBArray, baseV?: number,
   /** @default 'wcag2' */
   method?: ContrastFormula): number
 
-
-// FIXME Uncertain typing. Examine object at runtime to verify accuracy
-interface ContrastColor extends Color { background: ReturnType<typeof convertColorValue> }
-
 interface UpdateColorOptions extends Partial<ColorBase> {
   /**
    * The current name of the color to be updated.
@@ -103,13 +153,21 @@ export function minPositive(r: number[], formula: ContrastFormula): number
 export function ratioName(r: number[], formula: ContrastFormula): number[]
 
 export class Theme implements Required<ThemeBase> {
-  constructor({ colors, backgroundColor,
+  constructor({
+    colors,
+    backgroundColor,
     lightness,
-    /** @default 1 */
+    /**
+     * @default 1
+     */
     contrast,
-    /** @default 100 */
+    /**
+     * @default 100
+     */
     saturation,
-    /** @default 'HEX' */
+    /**
+     * @default 'HEX'
+     */
     output,
     /** @default 'wcag2' */
     formula }: ThemeBase)
@@ -125,42 +183,188 @@ export class Theme implements Required<ThemeBase> {
   readonly backgroundColorValue: number
 
   /**
-   * Array to be populated with JSON objects for each color, including names & contrast values
+   * Each color is an object named by user-defined value (eg `name: 'gray'`). `values` array consists of all generated color values for the color, with properties name, contrast, and value.
+   * @example
+   * [
+      { background: "#e0e0e0" },
+      {
+        name: 'gray',
+        values: [
+          {name: "gray100", contrast: 1, value: "#e0e0e0"},
+          {name: "gray200", contrast: 2, value: "#a0a0a0"},
+          {name: "gray300", contrast: 3, value: "#808080"},
+          {name: "gray400", contrast: 4.5, value: "#646464"}
+        ]
+      },
+      {
+        name: 'blue',
+        values: [
+          {name: "blue100", contrast: 2, value: "#b18cff"},
+          {name: "blue200", contrast: 3, value: "#8d63ff"},
+          {name: "blue300", contrast: 4.5, value: "#623aff"},
+          {name: "blue400", contrast: 8, value: "#1c0ad1"}
+        ]
+      }
+    ]
    */
-  // FIXME Uncertain typing. Examine object at runtime to verify accuracy
-  readonly contrastColors: ContrastColor[]
+  readonly contrastColors: [
+    ContrastColorBackground,
+    ...ContrastColor[]
+  ]
 
-  /*  Objext to be populated with flat list of all color values as named key-value pairs */
-  // FIXME Uncertain typing. Examine object at runtime to verify accuracy
-  readonly contrastColorPairs: ContrastColor
-
-  /*  Array to be populated with flat list of all color values */
-  // FIXME Uncertain typing. Examine object at runtime to verify accuracy
-  readonly contrastColorValues: any[]
-
-  /** Add individual new colors */
-  set addColor(arg: Color)
-  /** Remove individual colors */
-  set removeColor(arg: Color)
   /**
-   * Modify individual colors
+   * Simplified format as an object of key-value pairs. Property is equal to the {@link RatiosArray generated} or {@link RatiosObject user-defined name} for each generated value.
+   * @example {
+   *   "gray100": "#e0e0e0",
+   *   "gray200": "#a0a0a0",
+   *   "gray300": "#808080",
+   *   "gray400": "#646464",
+   *   "blue100": "#b18cff",
+   *   "blue200": "#8d63ff",
+   *   "blue300": "#623aff",
+   *   "blue400": "#1c0ad1"
+   * }
+   *
+   */
+  readonly contrastColorPairs: Record<string, HexColor>
+
+  /**
+   * All color values in a flat array.
+   * @example [ "#e0e0e0", "#a0a0a0", "#808080", "#646464", "#b18cff", "#8d63ff", "#623aff", "#1c0ad1" ]
+   */
+  readonly contrastColorValues: HexColor[]
+
+  /**
+   * Add a {@link Color} to the theme
    * @example ```
-   * // changing the name of a color:
-   * {color: 'blue', name: 'cerulean'}
+   * const red = new Color({...})
+   * theme.addColor = red;
+   * ```
+   */
+  set addColor(arg: Color)
+  /**
+   * Remove a {@link Color} from an existing theme. Accepts an object with the Color's name and value, or by passing the Color class itself.
+   * @example ```
+   * // Remove via color name
+    theme.removeColor = {name: 'Red'};
+   * ```
+   * @example ```
+   * // Remove via Color class
+   * const red = new Color({...})
+   * theme.removeColor = red;
+   * ```
+   */
+  set removeColor(arg: Color | { name: string })
+  /**
+   * Update a {@link Color} via its setters from the theme. Accepts an object with the name of the color you wish to modify, followed by the property and the new value you wish to modify.
+   * @example ```
+   * // Change the colors ratios
+   * theme.updateColor = {name: 'red', ratios: [3, 4.5, 7]};
+   * ```
+   * @example ```
+   * // Change the colors colorKeys
+   * theme.updateColor = {name: 'red', colorKeys: ['#ff0000']};
+   * ```
+   * @example ```
+   * // Change the color's name
+   * theme.updateColor = {name: 'red', name: 'Crimson'};
    * ```
    */
   set updateColor(arg: UpdateColorOptions | UpdateColorOptions[])
 }
 
+/**
+ * When passing a flat array of target ratios, the output colors in your Theme will be generated by concatenating the color name (eg "Blue") with numeric increments. Colors with a positive contrast ratio with the base (ie, 2:1) will be named in increments of 100. For example, `gray100`, `gray200`.
+ *
+ * Colors with a negative contrast ratio with the base (ie -2:1) will be named in increments less than 100 and based on the number of negative values declared. For example, if there are 3 negative values `[-1.4, -1.3, -1.2, 1, 2, 3]`, the name for those values will be incremented by 100/4 (length plus one to avoid a `0` value), such as `gray25`, `gray50`, and `gray75`.
+ * @example ```
+ * new Color({
+    name: 'blue',
+    colorKeys: ['#5CDBFF', '#0000FF'],
+    colorSpace: 'LCH',
+    ratios: [3, 4.5]
+  });
+  // Returns:
+  [
+    {
+      name: 'blue',
+      values: [
+        {name: "blue100", contrast: 3, value: "#8d63ff"},
+        {name: "blue200", contrast: 4.5, value: "#623aff"}
+      ]
+    }
+  ]
+  ```
+ */
+type RatiosArray = number[]
 
-type Ratios = number[] | Record<string, number>
+/**
+ * When defining ratios as an object with key-value pairs, you define what name will be output in your Leonardo theme.
+ *
+ * @example ```
+ * new Color({
+    name: 'blue',
+    colorKeys: ['#5CDBFF', '#0000FF'],
+    colorSpace: 'LCH',
+    ratios: {
+      'blue--largeText': 3,
+      'blue--normalText': 4.5
+    }
+  });
+  // Returns:
+  [
+    {
+      name: 'blue',
+      values: [
+        {name: "blue--largeText", contrast: 3, value: "#8d63ff"},
+        {name: "blue--normalText", contrast: 4.5, value: "#623aff"}
+      ]
+    }
+  ]
+ * ```
+ */
+type RatiosObject = Record<string, number>
+
+interface ContrastColorBackground {
+  background: HexColor
+}
+
+interface ContrastColor {
+  name: string
+  values: ContrastColorValue[]
+}
+
+interface ContrastColorValue {
+  name: string,
+  contrast: number
+  value: HexColor
+}
+
 
 interface ThemeBase {
+  /**
+   * List of {@link Color} classes to generate theme colors for.
+   */
   colors: Color[]
-  backgroundColor: Color
+  /**
+   * A single {@link BackgroundColor} class is required.
+   */
+  backgroundColor: BackgroundColor
+  /**
+   * Value from 0-100 for desired lightness of generated theme background color (whole number).
+   */
   lightness: number
+  /**
+   * Multiplier to increase or decrease contrast for all theme colors.
+   */
   contrast?: number
+  /**
+   * Value from 0-100 for decreasing saturation of all theme colors.
+   */
   saturation?: number
+  /**
+   * Desired color output format.
+   */
   output?: Colorspace
   formula?: ContrastFormula
 }
